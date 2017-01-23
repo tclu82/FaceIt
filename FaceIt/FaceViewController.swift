1e1222d9137946d0ed4f936009005303c03ef732
Zac <tclu82@uw.edu>
2017-01-23 01:29:22 -0800

    update eyes and mouth

---
+++ FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by Zac on 1/23/17.
+//  Copyright © 2017 Zac. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+    
+    
+
+}
